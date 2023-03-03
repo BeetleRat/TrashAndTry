0895143f22360254fbd53d7e2866ddc735e60359
beetlerat <beetlerat@yandex.ru>
2023-03-03 16:45:44 +0300

    Первый вариант решения

--- Saber/Java/src/main/java/ru/beetlerat/saber/Main.java
+++ Saber/Java/src/main/java/ru/beetlerat/saber/Main.java
@@ -1,7 +1,46 @@
 package main.java.ru.beetlerat.saber;
 
-public class Main {
-    public static void main(String[] args){
+import main.java.ru.beetlerat.saber.lists.ListBinSerialize;
+import main.java.ru.beetlerat.saber.lists.ListRand;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean isNewList = false;
+
+        ListRand list = createRandList(isNewList);
+        fileWork(isNewList, list,"src/main/resources/list.txt");
+    }
+
+    private static ListRand createRandList(boolean isNewList) {
+        ListRand list = new ListBinSerialize();
+        if (isNewList) {
+            list.add("Peter", "Vasiliy", "George", "Ivan", "Egor");
+            list.add("Sofia", true);
+        }
+        return list;
+    }
+
+    private static void fileWork(boolean serializeNewList, ListRand list, String fileName) {
+        try {
+            if (serializeNewList) {
+                FileOutputStream fos = new FileOutputStream(fileName);
+                list.Serialize(fos);
+                fos.close();
+            } else {
+                FileInputStream fis = new FileInputStream(fileName);
+                list.Deserialize(fis);
+                fis.close();
+            }
+            System.out.printf("List: %s\n", list);
+        } catch (FileNotFoundException e) {
+            System.out.println("Open file exception");
+        } catch (IOException e) {
+            System.out.println("Close file exception");
+        }
     }
 }
--- Saber/Java/src/main/java/ru/beetlerat/saber/lists/ListRand.java
+++ Saber/Java/src/main/java/ru/beetlerat/saber/lists/ListRand.java
@@ -1,18 +1,104 @@
 package main.java.ru.beetlerat.saber.lists;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.*;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class ListRand {
+public abstract class ListRand {
     public ListNode Head;
     public ListNode Tail;
     public int Count;
-    public void Serialize(FileOutputStream s)
-    {
+
+    public void Serialize(FileOutputStream s) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(s);
+            recursiveSerialize(Head, oos);
+            oos.close();
+        } catch (IOException e) {
+            System.out.println("ObjectOutputStream exception: " + e);
+        }
     }
 
-    public void Deserialize(FileInputStream s){
+    protected abstract void recursiveSerialize(ListNode currentNode, ObjectOutputStream oos) throws IOException;
 
+    public abstract void Deserialize(FileInputStream s);
+
+    public void add(String data) {
+        ListNode node = new ListNode();
+        node.Data = data;
+        if (Head == null) {
+            Head = node;
+            Tail = node;
+            Count = 0;
+        } else {
+            Tail.Next = node;
+            node.Prev = Tail;
+            Tail = node;
+        }
+        Count++;
     }
 
+    public void add(String data, boolean recalculateRandomPointer) {
+        add(data);
+        if (recalculateRandomPointer) {
+            recalculateRandomPointer();
+        }
+    }
+
+    public void add(String... data) {
+        for (String oneString : data) {
+            add(oneString);
+        }
+    }
+
+    public void recalculateRandomPointer() {
+        if (Head == null) {
+            return;
+        }
+        ListNode currentNode = Head;
+        while (currentNode.Next != null) {
+            int randomIndex = ThreadLocalRandom.current().nextInt(Count + 1);
+            currentNode.Rand =
+                    randomIndex == Count
+                            ? null
+                            : getNodeByIndex(Head, randomIndex);
+            currentNode = currentNode.Next;
+        }
+    }
+
+    public void clear() {
+        clear(Head);
+    }
+
+    private void clear(ListNode currentNode) {
+        if (currentNode == null) {
+            return;
+        }
+        clear(currentNode.Next);
+        currentNode = null;
+    }
+
+    private ListNode getNodeByIndex(ListNode currentNode, int indexAfterThisNode) {
+        if (currentNode == null || indexAfterThisNode <= 0) {
+            return currentNode;
+        }
+        return getNodeByIndex(currentNode.Next, indexAfterThisNode - 1);
+    }
+
+    private StringBuilder getData(ListNode currentNode, StringBuilder dataCollector) {
+        if (currentNode == null) {
+            return dataCollector;
+        }
+        dataCollector.append(currentNode.Data).append(", ");
+        return getData(currentNode.Next, dataCollector);
+    }
+
+    public String toString() {
+        StringBuilder outputString = new StringBuilder().append("[");
+        outputString = getData(Head, outputString);
+        if (outputString.length() > 2) {
+            outputString.setLength(outputString.length() - 2);
+        }
+        outputString.append("]");
+        return outputString.toString();
+    }
 }
