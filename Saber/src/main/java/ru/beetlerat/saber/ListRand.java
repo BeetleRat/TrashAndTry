bc51b3374afe2895443b704008da292641d1ce69
beetlerat <beetlerat@yandex.ru>
2023-03-06 18:49:45 +0300

    Рефакторинг

--- Saber/src/main/java/ru/beetlerat/saber/ListRand.java
+++ Saber/src/main/java/ru/beetlerat/saber/ListRand.java
@@ -4,37 +4,26 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 
 // Класс для записи объекта в файл
-class NodeDTO {
+record NodeDTO(int ID, String data, int prevID, int nextID, int randID) {
     public static final int NO_LINK = -1;
-    public int ID; // Добавлено для поддержания ссылочной целостности при строковой реализации
-    public int PrevID;
-    public int NextID;
-    public int RandID;
-    public String Data;
 
-    public NodeDTO(int ID, String data, int prevID, int nextID, int randID) {
-        this.ID = ID;
-        this.PrevID = prevID;
-        this.NextID = nextID;
-        this.RandID = randID;
-        this.Data = data;
-    }
-
+    @Override
     public String toString() {
         StringBuilder json = new StringBuilder();
 
         json.append("{\n");
         json.append("   \"ID\": ").append(ID).append("\n");
-        json.append("   \"Data\": ").append(Data).append("\n");
-        json.append("   \"PreviousID\": ").append(PrevID).append("\n");
-        json.append("   \"NextID\": ").append(NextID).append("\n");
-        json.append("   \"RandID\": ").append(RandID).append("\n");
+        json.append("   \"Data\": ").append(data).append("\n");
+        json.append("   \"PreviousID\": ").append(prevID).append("\n");
+        json.append("   \"NextID\": ").append(nextID).append("\n");
+        json.append("   \"RandID\": ").append(randID).append("\n");
         json.append("}\n");
 
         return json.toString();
     }
 }
 
+// Элемент списка
 class ListNode {
     public ListNode Prev;
     public ListNode Next;
@@ -42,18 +31,23 @@ class ListNode {
     public String Data;
 }
 
+
 public class ListRand {
     public ListNode Head;
     public ListNode Tail;
     public int Count;
 
+    public void clear() {
+        Count = 0;
+        Head = null;
+        Tail = null;
+    }
+
     public void Serialize(FileOutputStream s) {
         try {
-            BufferedWriter writeFileBuffer =
-                    new BufferedWriter(
-                            new OutputStreamWriter(
-                                    s, StandardCharsets.UTF_8)
-                    );
+            BufferedWriter writeFileBuffer = new BufferedWriter(
+                    new OutputStreamWriter(s, StandardCharsets.UTF_8)
+            );
 
             writeFileBuffer.write("Count: " + Count + "\n");
             executeSerialize(writeFileBuffer);
@@ -65,6 +59,12 @@ public class ListRand {
 
     }
 
+    public void Deserialize(FileInputStream s) {
+        clear();
+        int[] randLinks = readListFromFile(s);
+        setRandPointers(randLinks);
+    }
+
     private void executeSerialize(BufferedWriter writeFileBuffer) throws IOException {
         ListNode saveElement = Head;
         int saveElementIndex = 0;
@@ -73,17 +73,13 @@ public class ListRand {
         while (saveElementIndex < Count) {
 
             if (saveElement.Rand == null) {
-
-                // Сразу записываем DTO в файл
-                writeFileBuffer.write(
-                        new NodeDTO(
-                                saveElementIndex,
-                                saveElement.Data,
-                                saveElementIndex - 1,
-                                saveElement.Next == null ? -1 : saveElementIndex + 1,
-                                NodeDTO.NO_LINK
-                        ).toString()
+                NodeDTO nodeDTO = new NodeDTO(
+                        saveElementIndex, saveElement.Data, saveElementIndex - 1,
+                        saveElement.Next == null ? -1 : saveElementIndex + 1, NodeDTO.NO_LINK
                 );
+
+                // Записываем DTO в файл
+                writeFileBuffer.write(nodeDTO.toString());
 
                 saveElementIndex++;
                 saveElement = saveElement.Next;
@@ -96,17 +92,13 @@ public class ListRand {
                 }
 
                 if (linkElement == saveElement.Rand) {
-
-                    // Сразу записываем DTO в файл
-                    writeFileBuffer.write(
-                            new NodeDTO(
-                                    saveElementIndex,
-                                    saveElement.Data,
-                                    saveElementIndex - 1,
-                                    saveElement.Next == null ? -1 : saveElementIndex + 1,
-                                    linkElementIndex
-                            ).toString()
+                    NodeDTO nodeDTO = new NodeDTO(
+                            saveElementIndex, saveElement.Data, saveElementIndex - 1,
+                            saveElement.Next == null ? -1 : saveElementIndex + 1, linkElementIndex
                     );
+
+                    // Записываем DTO в файл
+                    writeFileBuffer.write(nodeDTO.toString());
 
                     saveElementIndex++;
                     saveElement = saveElement.Next;
@@ -120,49 +112,13 @@ public class ListRand {
         }
     }
 
-    public void Deserialize(FileInputStream s) {
-        clear();
-        int[] randLinks = readListFromFile(s);
-        setRandPointers(randLinks);
-    }
-
-    private void setRandPointers(int[] randLinks) {
-        ListNode elementWithBrokenLink = Head;
-        int elementWithBrokenLinkIndex = 0;
-        ListNode currentElement = Head;
-        int currentElementIndex = 0;
-
-        while (elementWithBrokenLinkIndex < Count) {
-
-            if (randLinks[elementWithBrokenLinkIndex] != NodeDTO.NO_LINK) {
-
-                if (currentElementIndex == randLinks[elementWithBrokenLinkIndex]) {
-                    elementWithBrokenLink.Rand = currentElement;
-
-                    currentElement = Head;
-                    currentElementIndex = 0;
-
-                    elementWithBrokenLinkIndex++;
-                    elementWithBrokenLink = elementWithBrokenLink.Next;
-                } else {
-                    currentElementIndex++;
-                    currentElement = currentElement.Next;
-                }
-            } else {
-                elementWithBrokenLink = elementWithBrokenLink.Next;
-                elementWithBrokenLinkIndex++;
-            }
-        }
-    }
-
     private int[] readListFromFile(FileInputStream s) {
+        // Массив ссылок на rand в каждом элементе
         int[] randLinks = null;
         try {
-            BufferedReader readFileBuffer =
-                    new BufferedReader(
-                            new InputStreamReader(
-                                    s, StandardCharsets.UTF_8)
-                    );
+            BufferedReader readFileBuffer = new BufferedReader(
+                    new InputStreamReader(s, StandardCharsets.UTF_8)
+            );
 
             String oneLine;
             StringBuilder json = new StringBuilder();
@@ -195,6 +151,35 @@ public class ListRand {
         return randLinks;
     }
 
+    private void setRandPointers(int[] randLinks) {
+        ListNode elementWithBrokenLink = Head;
+        int elementWithBrokenLinkIndex = 0;
+        ListNode currentElement = Head;
+        int currentElementIndex = 0;
+
+        while (elementWithBrokenLinkIndex < Count) {
+
+            if (randLinks[elementWithBrokenLinkIndex] != NodeDTO.NO_LINK) {
+
+                if (currentElementIndex == randLinks[elementWithBrokenLinkIndex]) {
+                    elementWithBrokenLink.Rand = currentElement;
+
+                    currentElement = Head;
+                    currentElementIndex = 0;
+
+                    elementWithBrokenLinkIndex++;
+                    elementWithBrokenLink = elementWithBrokenLink.Next;
+                } else {
+                    currentElementIndex++;
+                    currentElement = currentElement.Next;
+                }
+            } else {
+                elementWithBrokenLink = elementWithBrokenLink.Next;
+                elementWithBrokenLinkIndex++;
+            }
+        }
+    }
+
     private boolean accumulateJSON(StringBuilder json, String oneLine, int[] randLinks) {
         // Накапливаем строки в JSON
         json.append(oneLine).append("\n");
@@ -203,7 +188,7 @@ public class ListRand {
             // Парсим строковый JSON в DTO
             NodeDTO newNode = parseFromJSON(json.toString());
             // Если корректно спарсилось, сохраняем без ссылки Rand
-            if (newNode != null && newNode.ID >= 0 && newNode.ID < randLinks.length) {
+            if (newNode != null && newNode.ID() >= 0 && newNode.ID() < randLinks.length) {
                 addWithoutRandLink(newNode, randLinks);
             }
             // Очищаем строку JSON
@@ -213,17 +198,18 @@ public class ListRand {
         return true;
     }
 
+    // Добавить элемент в список при этом не восстанавливая rand ссылку
     private void addWithoutRandLink(NodeDTO nodeDTO, int[] randLinks) {
         ListNode newNode = new ListNode();
-        newNode.Data = nodeDTO.Data;
-        if (nodeDTO.ID == 0) {
+        newNode.Data = nodeDTO.data();
+        if (nodeDTO.ID() == 0) {
             Head = newNode;
         } else {
             newNode.Prev = Tail;
             Tail.Next = newNode;
         }
         Tail = newNode;
-        randLinks[nodeDTO.ID] = nodeDTO.RandID;
+        randLinks[nodeDTO.ID()] = nodeDTO.randID();
     }
 
     private NodeDTO parseFromJSON(String json) {
@@ -249,35 +235,29 @@ public class ListRand {
         return Integer.parseInt(parseLineToString(lines, lineNumber).trim());
     }
 
-    public void clear() {
-        clear(Head);
-        Count = 0;
-        Head = null;
-    }
-
-    private void clear(ListNode currentNode) {
-        if (currentNode == null) {
-            return;
-        }
-        if (currentNode == Tail) {
-            Tail = currentNode.Prev;
-        }
-        clear(currentNode.Next);
-        currentNode.Next = null;
-        currentNode.Rand = null;
-    }
-
-    private StringBuilder getData(ListNode currentNode, StringBuilder dataCollector, int currentID) {
+    private StringBuilder getListAsStringBuilder(ListNode currentNode,
+                                                 StringBuilder dataCollector,
+                                                 int currentID) {
         if (currentNode == null) {
             return dataCollector;
         }
-        dataCollector.append("{ID: ").append(currentID).append(", Data: ").append(currentNode.Data).append("; Rand: ").append(currentNode.Rand == null ? "null" : currentNode.Rand.Data).append("}, ");
-        return getData(currentNode.Next, dataCollector, currentID + 1);
+
+        dataCollector
+                .append("{ID: ")
+                .append(currentID)
+                .append(", Data: ")
+                .append(currentNode.Data)
+                .append("; Rand: ")
+                .append(currentNode.Rand == null ? "null" : currentNode.Rand.Data)
+                .append("}, ");
+
+        return getListAsStringBuilder(currentNode.Next, dataCollector, currentID + 1);
     }
 
+    @Override
     public String toString() {
         StringBuilder outputString = new StringBuilder().append("[");
-        outputString = getData(Head, outputString, 0);
+        outputString = getListAsStringBuilder(Head, outputString, 0);
         if (outputString.length() > 2) {
             outputString.setLength(outputString.length() - 2);
         }
