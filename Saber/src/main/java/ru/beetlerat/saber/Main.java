837aebbe17fce11d7ff60464d5f924f73a04fc1b
beetlerat <beetlerat@yandex.ru>
2023-03-06 13:24:13 +0300

    Выполненное задание в одном файле

--- Saber/src/main/java/ru/beetlerat/saber/Main.java
+++ Saber/src/main/java/ru/beetlerat/saber/Main.java
@@ -1,146 +1,21 @@
 package main.java.ru.beetlerat.saber;
 
-import main.java.ru.beetlerat.saber.lists.ListNode;
-import main.java.ru.beetlerat.saber.lists.ListRand;
-import main.java.ru.beetlerat.saber.lists.ListStringSerialize;
 
-import java.awt.*;
+import main.java.ru.beetlerat.saber.lists.ListRand;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
-    private static final int ADD_ELEMENT = 1, REMOVE_ELEMENT = 2, FIND_ELEMENT = 3, UPDATE_ELEMENT = 4, CLEAR_ARRAY = 5, CREATE_RANDOM_LINKS = 6, SERIALIZE_ARRAY = 7, DESERIALIZE_ARRAY = 8, CREATE_TEST_ARRAY = 9, EXIT = 0, NOT_CORRECT_NUMBER = -9999999;
 
     public static void main(String[] args) {
-        ListRand list = new ListStringSerialize();
-        Scanner inputData = new Scanner(System.in);
-        while (showMenu(list, inputData)) ;
-    }
-
-    private static boolean showMenu(ListRand list, Scanner inputData) {
-        String menuText = "List: " + list + "\n1)Add element to list;\n2)Remove element from list;\n3)Find element in list;\n4)Update element in list;\n5)Clear list;\n6)Create random links in list;\n7)Serialize list;\n8)Deserialize list;\n9)Create test list;\n0)Exit.\nInput action: ";
-        String fileName, newData = "";
-        int menuItem = -1;
-
-        System.out.print(menuText);
-        String answer = inputData.nextLine();
-        try {
-            menuItem = Integer.parseInt(answer.trim());
-        } catch (NumberFormatException e) {
-            menuItem = NOT_CORRECT_NUMBER;
-        }
-
-        printDivider();
-
-        switch (menuItem) {
-            case ADD_ELEMENT:
-                System.out.print("Enter new data: ");
-                newData = inputData.nextLine();
-                list.add(newData);
-                break;
-            case REMOVE_ELEMENT:
-                removeElementFromList(list, inputData);
-                break;
-            case FIND_ELEMENT:
-                findListElement(list, inputData);
-                break;
-            case UPDATE_ELEMENT:
-                updateListElement(list, inputData);
-                break;
-            case CLEAR_ARRAY:
-                list.clear();
-                System.out.println("List was cleared.");
-                break;
-            case CREATE_RANDOM_LINKS:
-                list.recalculateRandomPointer();
-                System.out.println("Random links was created.");
-                break;
-            case SERIALIZE_ARRAY:
-                System.out.print("Enter file name: ");
-                fileName = inputData.nextLine();
-                fileWork(true, list, fileName);
-                break;
-            case DESERIALIZE_ARRAY:
-                System.out.print("Enter file name: ");
-                fileName = inputData.nextLine();
-                fileWork(false, list, fileName);
-                break;
-            case CREATE_TEST_ARRAY:
-                list.clear();
-                list.add("Kirill", "Egor", "Evgeniy", "Georg", "Dante", "Maria", "Timur", "Alena", "Lev", "Vladimir");
-                list.add("Sofia", true);
-                System.out.printf("Previous list was cleared.\nNew test list was created.\nTest list count=%d.", list.Count);
-                break;
-            case EXIT:
-                System.out.println("Exit from program...");
-                return false;
-            default:
-                System.out.println("Action number is not correct.");
-        }
-        printDivider();
-        return true;
-    }
-
-    private static void removeElementFromList(ListRand list, Scanner inputData) {
-        int elementIndex;
-        System.out.print("Enter element index: ");
-        elementIndex = readInt(inputData);
-        if (list.Count <= elementIndex || elementIndex < 0) {
-            System.out.println("Index is not correct.");
-        } else {
-            if (list.remove(elementIndex)) {
-                System.out.println("Element " + elementIndex + " was removed.");
-            } else {
-                System.out.println("Element " + elementIndex + " not found");
-            }
-        }
-    }
-
-    private static void findListElement(ListRand list, Scanner inputData) {
-        int elementIndex = readListIndex(list, inputData);
-        if (elementIndex != NOT_CORRECT_NUMBER) {
-            ListNode foundElement = list.get(elementIndex);
-            if (foundElement == null) {
-                System.out.println("Element not found");
-            } else {
-                System.out.println("Element: {ID: " + elementIndex + ", Data: " + foundElement.Data + "; Rand: " + (foundElement.Rand == null ? "null" : foundElement.Rand.Data + "}"));
-            }
-        }
-    }
-
-    private static void updateListElement(ListRand list, Scanner inputData) {
-        String newData;
-        int elementIndex = readListIndex(list, inputData);
-        if (elementIndex != NOT_CORRECT_NUMBER) {
-            System.out.print("Enter new data: ");
-            newData = inputData.nextLine();
-            list.set(elementIndex, newData);
-        }
-    }
-
-    private static int readListIndex(ListRand list, Scanner inputData) {
-        int elementIndex;
-        System.out.print("Enter element index: ");
-        elementIndex = readInt(inputData);
-        if (list.Count <= elementIndex || elementIndex < 0) {
-            System.out.println("Index is not correct.");
-            return NOT_CORRECT_NUMBER;
-        }
-        return elementIndex;
-    }
-
-    private static int readInt(Scanner inputData) {
-        int readIntFromConsole;
-        try {
-            String answer = inputData.nextLine();
-            readIntFromConsole = Integer.parseInt(answer.trim());
-        } catch (NumberFormatException e) {
-            return NOT_CORRECT_NUMBER;
-        }
-        return readIntFromConsole;
+        ListRand list = new ListRand();
+        System.out.println("New list was created: "+list);
+        fileWork(false,list,"src/main/resources/stringListFrom.txt");
+        System.out.println("List after deserialization: "+list);
+        fileWork(true,list,"src/main/resources/stringListTo.txt");
     }
 
     // Сериализация/Десериализация
@@ -163,10 +38,6 @@ public class Main {
         } catch (IOException e) {
             System.out.println("Close file exception: " + e);
         }
-    }
-
-    private static void printDivider() {
-        System.out.print("\n=======================================================================================================================\n");
     }
 
 }
--- Saber/src/main/java/ru/beetlerat/saber/lists/ListRand.java
+++ Saber/src/main/java/ru/beetlerat/saber/lists/ListRand.java
@@ -1,42 +1,252 @@
 package main.java.ru.beetlerat.saber.lists;
 
 import java.io.*;
-import java.util.concurrent.ThreadLocalRandom;
+import java.nio.charset.StandardCharsets;
 
-public abstract class ListRand {
+// Класс для записи объекта в файл
+class NodeDTO {
+    public static final int NO_LINK = -1;
+    public int ID; // Добавлено для поддержания ссылочной целостности при строковой реализации
+    public int PrevID;
+    public int NextID;
+    public int RandID;
+    public String Data;
+
+    public NodeDTO(int ID, String data, int prevID, int nextID, int randID) {
+        this.ID = ID;
+        this.PrevID = prevID;
+        this.NextID = nextID;
+        this.RandID = randID;
+        this.Data = data;
+    }
+
+    public String toString() {
+        StringBuilder json = new StringBuilder();
+
+        json.append("{\n");
+        json.append("   \"ID\": ").append(ID).append("\n");
+        json.append("   \"Data\": ").append(Data).append("\n");
+        json.append("   \"PreviousID\": ").append(PrevID).append("\n");
+        json.append("   \"NextID\": ").append(NextID).append("\n");
+        json.append("   \"RandID\": ").append(RandID).append("\n");
+        json.append("}\n");
+
+        return json.toString();
+    }
+}
+
+class ListNode {
+    public ListNode Prev;
+    public ListNode Next;
+    public ListNode Rand; // произвольный элемент внутри списка
+    public String Data;
+}
+
+public class ListRand {
     public ListNode Head;
     public ListNode Tail;
     public int Count;
 
-    public abstract void Serialize(FileOutputStream s);
+    public void Serialize(FileOutputStream s) {
+        try {
+            BufferedWriter writeFileBuffer =
+                    new BufferedWriter(
+                            new OutputStreamWriter(
+                                    s, StandardCharsets.UTF_8)
+                    );
 
-    public abstract void Deserialize(FileInputStream s);
+            writeFileBuffer.write("Count: " + Count + "\n");
+            executeSerialize(writeFileBuffer);
 
-    public void add(String data) {
-        ListNode node = new ListNode();
-        node.Data = data;
-        if (Head == null) {
-            Head = node;
-            Count = 0;
+            writeFileBuffer.close();
+        } catch (IOException e) {
+            System.out.println("Write to file exception: " + e);
+        }
+
+    }
+
+    private void executeSerialize(BufferedWriter writeFileBuffer) throws IOException {
+        ListNode saveElement = Head;
+        int saveElementIndex = 0;
+        ListNode linkElement = Head;
+        int linkElementIndex = 0;
+        while (saveElementIndex < Count) {
+
+            if (saveElement.Rand == null) {
+
+                // Сразу записываем DTO в файл
+                writeFileBuffer.write(
+                        new NodeDTO(
+                                saveElementIndex,
+                                saveElement.Data,
+                                saveElementIndex - 1,
+                                saveElement.Next == null ? -1 : saveElementIndex + 1,
+                                NodeDTO.NO_LINK
+                        ).toString()
+                );
+
+                saveElementIndex++;
+                saveElement = saveElement.Next;
+            } else {
+                // Удаление битой ссылки
+                if (linkElementIndex >= Count) {
+                    linkElementIndex = 0;
+                    linkElement = Head;
+                    saveElement.Rand = null;
+                }
+
+                if (linkElement == saveElement.Rand) {
+
+                    // Сразу записываем DTO в файл
+                    writeFileBuffer.write(
+                            new NodeDTO(
+                                    saveElementIndex,
+                                    saveElement.Data,
+                                    saveElementIndex - 1,
+                                    saveElement.Next == null ? -1 : saveElementIndex + 1,
+                                    linkElementIndex
+                            ).toString()
+                    );
+
+                    saveElementIndex++;
+                    saveElement = saveElement.Next;
+                    linkElement = Head;
+                    linkElementIndex = 0;
+                } else {
+                    linkElementIndex++;
+                    linkElement = linkElement.Next;
+                }
+            }
+        }
+    }
+
+    public void Deserialize(FileInputStream s) {
+        clear();
+        int[] randLinks = readListFromFile(s);
+        setRandPointers(randLinks);
+    }
+
+    private void setRandPointers(int[] randLinks) {
+        ListNode elementWithBrokenLink = Head;
+        int elementWithBrokenLinkIndex = 0;
+        ListNode currentElement = Head;
+        int currentElementIndex = 0;
+
+        while (elementWithBrokenLinkIndex < Count) {
+
+            if (randLinks[elementWithBrokenLinkIndex] != NodeDTO.NO_LINK) {
+
+                if (currentElementIndex == randLinks[elementWithBrokenLinkIndex]) {
+                    elementWithBrokenLink.Rand = currentElement;
+
+                    currentElement = Head;
+                    currentElementIndex = 0;
+
+                    elementWithBrokenLinkIndex++;
+                    elementWithBrokenLink = elementWithBrokenLink.Next;
+                } else {
+                    currentElementIndex++;
+                    currentElement = currentElement.Next;
+                }
+            } else {
+                elementWithBrokenLink = elementWithBrokenLink.Next;
+                elementWithBrokenLinkIndex++;
+            }
+        }
+    }
+
+    private int[] readListFromFile(FileInputStream s) {
+        int[] randLinks = null;
+        try {
+            BufferedReader readFileBuffer =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    s, StandardCharsets.UTF_8)
+                    );
+
+            String oneLine;
+            StringBuilder json = new StringBuilder();
+            boolean isJsonFound = false;
+            while ((oneLine = readFileBuffer.readLine()) != null) {
+                // Файл должен начинастья с "Count: "
+                if (randLinks == null) {
+                    if (oneLine.contains("Count: ")) {
+                        int count = Integer.parseInt(oneLine.substring(oneLine.indexOf(":") + 1).trim());
+                        randLinks = new int[count];
+                        Count = count;
+                    }
+                } else {
+                    // Проверка корректности сохраненного JSON
+                    if (oneLine.equals("{")) {
+                        isJsonFound = true;
+                    }
+                    if (isJsonFound) {
+                        isJsonFound = accumulateJSON(json, oneLine, randLinks);
+                    }
+                    if (oneLine.equals("}")) {
+                        isJsonFound = false;
+                    }
+                }
+            }
+            readFileBuffer.close();
+        } catch (IOException e) {
+            System.out.println("Read from file exception: " + e);
+        }
+        return randLinks;
+    }
+
+    private boolean accumulateJSON(StringBuilder json, String oneLine, int[] randLinks) {
+        // Накапливаем строки в JSON
+        json.append(oneLine).append("\n");
+        // Если в JSON 6 и больше строк
+        if (json.toString().split("\n").length >= 6) {
+            // Парсим строковый JSON в DTO
+            NodeDTO newNode = parseFromJSON(json.toString());
+            // Если корректно спарсилось, сохраняем без ссылки Rand
+            if (newNode != null && newNode.ID >= 0 && newNode.ID < randLinks.length) {
+                addWithoutRandLink(newNode, randLinks);
+            }
+            // Очищаем строку JSON
+            json.setLength(0);
+            return false;
+        }
+        return true;
+    }
+
+    private void addWithoutRandLink(NodeDTO nodeDTO, int[] randLinks) {
+        ListNode newNode = new ListNode();
+        newNode.Data = nodeDTO.Data;
+        if (nodeDTO.ID == 0) {
+            Head = newNode;
         } else {
-            Tail.Next = node;
-            node.Prev = Tail;
+            newNode.Prev = Tail;
+            Tail.Next = newNode;
         }
-        Tail = node;
-        Count++;
+        Tail = newNode;
+        randLinks[nodeDTO.ID] = nodeDTO.RandID;
     }
 
-    public void add(String data, boolean recalculateRandomPointer) {
-        add(data);
-        if (recalculateRandomPointer) {
-            recalculateRandomPointer();
+    private NodeDTO parseFromJSON(String json) {
+        String[] lines = json.split("\n");
+        if (lines.length != 6) {
+            return null;
         }
+
+        return new NodeDTO(
+                parseLineToInt(lines, 1),
+                parseLineToString(lines, 2),
+                parseLineToInt(lines, 3),
+                parseLineToInt(lines, 4),
+                parseLineToInt(lines, 5)
+        );
     }
 
-    public void add(String... data) {
-        for (String oneString : data) {
-            add(oneString);
-        }
+    private static String parseLineToString(String[] lines, int x) {
+        return lines[x].substring(lines[x].indexOf(':') + 2);
+    }
+
+    private static int parseLineToInt(String[] lines, int lineNumber) {
+        return Integer.parseInt(parseLineToString(lines, lineNumber).trim());
     }
 
     public void clear() {
@@ -55,57 +265,6 @@ public abstract class ListRand {
         clear(currentNode.Next);
         currentNode.Next = null;
         currentNode.Rand = null;
-    }
-
-    public ListNode get(int index) {
-        return getNodeByIndex(Head, index);
-    }
-
-    public boolean remove(int index) {
-        ListNode removeNode = getNodeByIndex(Head, index);
-        if (removeNode != null) {
-            Count--;
-            if (removeNode == Head) {
-                Head = removeNode.Next;
-            } else {
-                removeNode.Prev = removeNode.Next;
-            }
-            if (removeNode == Tail) {
-                Tail = removeNode.Prev;
-            }
-            removeNode = null;
-            return true;
-        }
-        return false;
-    }
-
-    public void set(int index, String newData) {
-        ListNode updatedNode = getNodeByIndex(Head, index);
-        if (updatedNode != null) {
-            updatedNode.Data = newData;
-        }
-    }
-
-    public void recalculateRandomPointer() {
-        if (Head == null) {
-            return;
-        }
-        ListNode currentNode = Head;
-        while (currentNode != null) {
-            int randomIndex = ThreadLocalRandom.current().nextInt(Count + 1);
-            currentNode.Rand =
-                    randomIndex == Count
-                            ? null
-                            : getNodeByIndex(Head, randomIndex);
-            currentNode = currentNode.Next;
-        }
-    }
-
-    private ListNode getNodeByIndex(ListNode currentNode, int indexAfterThisNode) {
-        if (currentNode == null || indexAfterThisNode <= 0) {
-            return currentNode;
-        }
-        return getNodeByIndex(currentNode.Next, indexAfterThisNode - 1);
     }
 
     private StringBuilder getData(ListNode currentNode, StringBuilder dataCollector, int currentID) {
